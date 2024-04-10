d9416cf1f790ff4520b6c6708ee5fe08c67f4896
SH4ND <miqueiasantos20@gmail.com>
2024-04-09 22:58:48 -0300

    fs: Guard kernelSU hooks
    Change-Id: Idc78bf254c7f7b4686611bba9afdb456bd27714a
    Signed-off-by: SH4ND <miqueiasantos20@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1528,10 +1528,10 @@ static int exec_binprm(struct linux_binprm *bprm)
 }
 
 
-// KernelSU hook
+#ifdef CONFIG_KSU
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			       void *envp, int *flags);
-
+#endif
 /*
  * sys_execve() executes a new program.
  */
@@ -1545,11 +1545,11 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
-
-        ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);  // call KSU hook first
+#ifdef CONFIG_KSU
+        ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+#endif
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
-
 	/*
 	 * We move the actual failure in case of RLIMIT_NPROC excess from
 	 * set*uid() to execve() because too many poorly written programs
